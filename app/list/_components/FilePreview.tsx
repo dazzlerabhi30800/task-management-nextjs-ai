9fb1a297407f478dc7711ce297c24784c2c2ab87
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-03-04 14:52:52 +0530

    removed unused declarations in filePreview.tsx

--- app/list/_components/FilePreview.tsx
+++ app/list/_components/FilePreview.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { file } from "@/public/store/TodoSlice";
 import { ArrowDown, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
